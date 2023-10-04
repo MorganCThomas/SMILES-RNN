985fe59af4c0be182f40c994fd5abfc8b701cb12
MorganCThomas <morganthomas263@gmail.com>
2023-10-04 11:15:40 +0200

    move scripts into dir, tidy up a bit

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author='Morgan Thomas',
     author_email='morganthomas263@gmail.com',
     description='A Generic SMILES-RNN, modified from and based off reinvent 2.0',
-    scripts=['sample_model.py', 'train_prior.py', 'fine_tune.py', 'reinforcement_learning.py',
+    scripts=['scripts/sample_model.py', 'scripts/train_prior.py', 'scripts/fine_tune.py', 'scripts/reinforcement_learning.py',
      'utility_scripts/deep_smiles.py', 'utility_scripts/randomize_smiles.py']
 )
